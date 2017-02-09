5aae69b4f220af2388b058bba9bc077638c9fdba
Tyler A <tyler.atiburcio@gmail.com>
2017-02-08 19:56:46 -1000

    HW3 GM

--- src/HW3/RecursiveInsertionSort.java
+++ src/HW3/RecursiveInsertionSort.java
@@ -1,8 +1,11 @@
 package HW3;
 
-/**
- *
- * @author Tiny7
+/**Tyler Atiburcio
+ *CSCI 3101
+ * HW-3; Recursive Insertion SOrt
+ * @author Tyler Atiburcio
+ * @DATE 2/8/14
+ * @version 1
  */
 public class RecursiveInsertionSort {
     
@@ -20,14 +23,28 @@ public class RecursiveInsertionSort {
             System.out.println(TEST[i]);
         }
     }
-    
+    /**Recursive method that sorts an array using InsertionSort Algorithm
+     * PreCondition: Array index is not 0
+     * PostCondition: Array is Sorted when index is 0
+     * @param index Current index to sort to
+     */
     public static void recursiveInsertion(int index)
     {
         if(index == 0) return;
         recursiveInsertion(index-1);
+        
+        //Begin debug statements to confirm that array is sorted recursively
+        System.out.println("Step: " + index);
+        for (int i = 0; i < TEST.length; i++) {
+            System.out.println(TEST[i]);
+        }
+        System.out.println("");
+        //End debug statements to confirm that array is sorted recursively
+        
         int j;
         int key;
         int i;
+        //Sort from 0 to index (sorted side of the array)
         for (j = 0; j <= index; j++) {
             key = TEST[j];
             for (i = j-1; (i >=0) && (TEST[i] > key); i--) {
